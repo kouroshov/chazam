07a303671571dc2619a834c27d5d5d0bffad52af
Kourosh Shahi <127735340+kouroshov@users.noreply.github.com>
2026-03-19 13:43:41 +0100

    creation des functions pour certain messages particuliers

--- second_chatbot.py
+++ second_chatbot.py
@@ -1,3 +1,20 @@
+import re
+
+def addition(match):
+    a = int(match.group(1))
+    b = int(match.group(2))
+    somme = a + b
+    return [f"le resultat est {somme}", somme]
+
+def info_date(match):
+    jour, mois, annee = match.group(1), match.group(2), match.group(3)
+    return [f"Le {jour}/{mois}/{annee} a été enregistré", f"{jour}/{mois}/{annee}"]
+
+def calcul_age(match):
+    age = match.group(1)
+    return [f"Cool, t'as {age} ans!", age]
+
+
 regle_v2 = [
     ["date", r"(\d{2})/(\d{2})/(\d{4})", None, info_date, 8, True],
     ["age_reg", r"(\d+)\s*ans?", None, calcul_age, 7, True],
